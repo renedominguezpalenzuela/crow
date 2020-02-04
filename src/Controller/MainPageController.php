d393826e7ded1e02c790f8a2eb48d9f0dd747582
renedominguezpalenzuela <renedp1975@gmail.com>
2020-02-04 16:02:31 -0400

    creand pagina principal

---
+++ src/Controller/MainPageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainPageController extends AbstractController
+{
+    /**
+     * @Route("/mainpage", name="main_page")
+     */
+    public function index()
+    {
+        return $this->render('main_page/index.html.twig', [
+            'controller_name' => 'MainPageController',
+        ]);
+    }
+}

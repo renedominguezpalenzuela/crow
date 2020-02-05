d18fc0c064ce6f7c965f4ab218c3f06a746cedf3
renedominguezpalenzuela <renedp1975@gmail.com>
2020-02-05 11:29:42 -0400

    Pagina Inicial con JS

---
+++ assets/js/main_page/main_page.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#boton1').click(function() {
+        alert('H ola mundo1');
+    })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-var Encore = require('@symfony/webpack-encore');
+var Encore = require('@symfony/webpack-encore')
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
 }
 
 Encore
@@ -12,7 +12,7 @@ Encore
     // public path used by the web server to access the output path
     .setPublicPath('build')
     // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
+    // .setManifestKeyPrefix('build/')
 
     /*
      * ENTRY CONFIG
@@ -24,9 +24,11 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('js/app', './assets/js/app.js')
+    .addEntry('js/main_page/main_page', ['./assets/js/main_page/main_page.js','./assets/js/main_page/otro.js'])
+    //.addEntry('js/main_page/main_page', './assets/js/main_page/otro.js')
     .addStyleEntry('css/app', './assets/css/global.scss')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+    // .addEntry('page1', './assets/js/page1.js')
+    // .addEntry('page2', './assets/js/page2.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -49,27 +51,26 @@ Encore
     .enableVersioning(Encore.isProduction())
 
     // enables @babel/preset-env polyfills
-    .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = 3;
+    .configureBabelPresetEnv(config => {
+        config.useBuiltIns = 'usage'
+        config.corejs = 3
     })
 
     // enables Sass/SCSS support
     .enableSassLoader()
 
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+// uncomment if you use TypeScript
+// .enableTypeScriptLoader()
 
-    // uncomment to get integrity="..." attributes on your script & link tags
-    // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
+// uncomment to get integrity="..." attributes on your script & link tags
+// requires WebpackEncoreBundle 1.4 or higher
+// .enableIntegrityHashes(Encore.isProduction())
 
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+// uncomment if you're having problems with a jQuery plugin
+ .autoProvidejQuery()
 
-    // uncomment if you use API Platform Admin (composer req api-admin)
-    //.enableReactPreset()
-    //.addEntry('admin', './assets/js/admin.js')
-;
+// uncomment if you use API Platform Admin (composer req api-admin)
+// .enableReactPreset()
+// .addEntry('admin', './assets/js/admin.js')
 
-module.exports = Encore.getWebpackConfig();
+module.exports = Encore.getWebpackConfig()
